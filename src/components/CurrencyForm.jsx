9a0b0a1317fb2bb4a57ecd2c1445c6eeed2fe216
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-10-02 10:34:02 +0100

    added calculate button and timestamp

--- src/components/CurrencyForm.jsx
+++ src/components/CurrencyForm.jsx
@@ -5,6 +5,8 @@ import { faDownLong } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import "./CurrencyForm.css";
 import { currencyList } from "./currencyList";
+import Button from "react-bootstrap/Button";
+import Timestamp from "react-timestamp";
 
 export default function CurrencyForm() {
   const ratio = 1.5;
@@ -51,13 +53,21 @@ export default function CurrencyForm() {
         <FontAwesomeIcon icon={faDownLong} size="3x" />
 
         <div className="row justify-content-center align-items-center mt-3 mb-5">
-          <div className="col-lg-4 text-left pl-4 border">
+          {/* <div className="col-lg-4 text-left pl-4 border">
             <p>{from * ratio}</p>
-          </div>
+          </div> */}
           <CurrencyOptions currencies={currencies} />
+          <Button variant="primary">Calculate</Button>
         </div>
       </form>
       {/* const url ="https://api.freecurrencyapi.com/v1/latest?apikey=YOUR-APIKEY" */}
+      <div className="result">
+        <p>You will have {from * ratio} GBP</p>
+        <p>Rate: {ratio}</p>
+        <p>
+          <Timestamp date={Date()} />
+        </p>
+      </div>
     </div>
   );
 }
